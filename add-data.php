e8c4b23230aded7380f599989900333104cb4a69
yourusername <jakaprayudha3@gmail.com>
2024-02-12 22:58:03 +0700

    Add Data Barang

---
+++ add-data.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+
+<body>
+   <form action="">
+      <table>
+         <tr>
+            <td>Kode</td>
+            <td>
+               <input type="text" name="" id="">
+            </td>
+         </tr>
+         <tr>
+            <td>Nama Barang</td>
+            <td>
+               <input type="text" name="" id="">
+            </td>
+         </tr>
+         <tr>
+            <td>Satuan</td>
+            <td>
+               <input type="text" name="" id="">
+            </td>
+         </tr>
+         <tr>
+            <td>Harga Jual</td>
+            <td>
+               <input type="number" name="" id="">
+            </td>
+         </tr>
+         <tr>
+            <td><button>Simpan</button></td>
+            <td><button type="reset">Reset</button></td>
+            <td>
+               <a href="index.php">
+                  <button type="button">Kembali</button>
+               </a>
+            </td>
+         </tr>
+      </table>
+   </form>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -9,6 +9,27 @@
 
 <body>
    <h1>Data Barang</h1>
+   <a href="add-data.php">
+      <button>Tambah Data</button>
+   </a>
+   <table>
+      <thead>
+         <tr>
+            <th>Kode</th>
+            <th>Nama Barang</th>
+            <th>Satuan</th>
+            <th>Harga Jual</th>
+         </tr>
+      </thead>
+      <tbody>
+         <tr>
+            <td>09123</td>
+            <td>Flasdisk 16GB</td>
+            <td>Pcs</td>
+            <td>75.000</td>
+         </tr>
+      </tbody>
+   </table>
 </body>
 
 </html>
