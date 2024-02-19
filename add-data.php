ac3510c63c4cc3927e7e709a9d3765db16bab74f
yourusername <jakaprayudha3@gmail.com>
2024-02-19 22:19:37 +0700

    Update Fitur Add Data

--- add-data.php
+++ add-data.php
@@ -40,13 +40,17 @@
                <input type="number" class="form-control" id="inputPassword">
             </div>
          </div>
+
          <div class="mb-3 row">
             <label for="inputPassword" class="col-sm-2 col-form-label"></label>
             <div class="col-sm-10">
                <button class="btn btn-primary">Simpan</button>
-               <button class="btn btn-dark">Kembali</button>
+               <a href="index.php">
+                  <button type="button" class="btn btn-light">Kembali</button>
+               </a>
             </div>
          </div>
+
 
       </form>
    </div>
